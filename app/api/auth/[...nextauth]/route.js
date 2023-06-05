1734ef15b838af46dd2cace2c9bb0a27402f64bf
YuryHouse <yury.house@yahoo.com>
2023-06-05 16:31:45 +0300

    feature: were created google oAuth2 and database.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,16 +4,20 @@ import GoogleProvider from 'next-auth/providers/google';
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
 
     },
     async signIn({ profile }){
-
+        try {
+            
+        } catch (e) {
+            
+        }
     }
 })
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
